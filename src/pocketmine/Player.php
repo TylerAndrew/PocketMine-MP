9afad65b748318c9858ec8a75d80374d0ef56c28
Dylan K. Taylor <odigiman@gmail.com>
2017-04-01 19:20:30 +0100

    Some protocol changes for 1.1.0.0

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -3312,8 +3312,8 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 		}
 		$this->craftingType = 0;
 		$commandText = $packet->command;
-		if($packet->args !== null){
-			foreach($packet->args as $arg){ //command ordering will be an issue
+		if($packet->inputJson !== null){
+			foreach($packet->inputJson as $arg){ //command ordering will be an issue
 				$commandText .= " " . $arg;
 			}
 		}
@@ -3403,7 +3403,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 
 		$this->server->getPluginManager()->callEvent($ev = new DataPacketReceiveEvent($this, $packet));
 		if(!$ev->isCancelled() and !$packet->handle($this)){
-			$this->server->getLogger()->debug("Unhandled " . $packet->getName() . " received from " . $this->getName() . ": 0x" . bin2hex($packet->buffer));
+			$this->server->getLogger()->debug("Unhandled " . get_class($packet) . " received from " . $this->getName() . ": " . bin2hex($packet->buffer));
 		}
 
 		$timings->stopTiming();
