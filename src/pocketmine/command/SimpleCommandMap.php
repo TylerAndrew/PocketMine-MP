1b6507cc7d18e2ebe5e19b2ab1be4c2c8bb91bfb
HiggsInc <adrian@adrianhiggs.com>
2017-05-02 15:54:58 +0200

    Update SimpleCommandMap.php (#64)
    Correction to TransferServerCommand which was originally in lowercase

--- src/pocketmine/command/SimpleCommandMap.php
+++ src/pocketmine/command/SimpleCommandMap.php
@@ -58,7 +58,7 @@ use pocketmine\command\defaults\TeleportCommand;
 use pocketmine\command\defaults\TellCommand;
 use pocketmine\command\defaults\TimeCommand;
 use pocketmine\command\defaults\TimingsCommand;
-use pocketmine\command\defaults\TransferserverCommand;
+use pocketmine\command\defaults\TransferServerCommand;
 use pocketmine\command\defaults\VanillaCommand;
 use pocketmine\command\defaults\VersionCommand;
 use pocketmine\command\defaults\WhitelistCommand;
@@ -117,7 +117,7 @@ class SimpleCommandMap implements CommandMap{
 		$this->register("pocketmine", new TeleportCommand("tp"));
 		$this->register("pocketmine", new TimeCommand("time"));
 		$this->register("pocketmine", new TimingsCommand("timings"));
-		$this->register("pocketmine", new TransferserverCommand("transferserver"));
+		$this->register("pocketmine", new TransferServerCommand("transferserver"));
 		$this->register("pocketmine", new ReloadCommand("reload"));
 
 		if($this->server->getProperty("debug.commands", false)){
