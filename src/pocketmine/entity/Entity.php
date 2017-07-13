18f2f9a75133b1f1bc9ff2505012443d7e2a716a
thebigsmileXD <thebigsmilexd@gmail.com>
2017-07-13 12:47:33 +0200

    Fix #87

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -1045,11 +1045,11 @@ abstract class Entity extends Location implements Metadatable{
 		}
 	}
 
-	public function getAbsorption() : int{
+	public function getAbsorption() : float{
 		return 0;
 	}
 
-	public function setAbsorption(int $absorption){
+	public function setAbsorption(float $absorption){
 
 	}
 
